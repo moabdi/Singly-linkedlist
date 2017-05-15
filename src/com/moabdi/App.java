b4f0b89a7b3b32dbf37177e4fa9811776ad408ed
Mostapha ABDI <abdimostapha@gmail.com>
2017-05-15 11:11:35 +0200

    Update App.java

--- src/com/moabdi/App.java
+++ src/com/moabdi/App.java
@@ -4,16 +4,26 @@ public class App {
 
 	public static void main(String[] args) {
 		SinglyLinkedList list = new SinglyLinkedList();
+		
 		System.out.println("Append to singly linked list");
-//		list.append(11);
-//		list.append(12);
-//		list.append(13);
-//		list.append(14);
-//		list.append(15);
-//		list.append(16);
-//		list.append(17);
-		System.out.println("Remove ");
+ 		list.append(11);
+ 		list.append(12);
+ 		list.append(13);
+ 		list.append(14);
+ 		list.append(15);
+ 		list.append(16);
+ 		list.append(17);
+		
+		System.out.println("Remove Tail ");
 		list.removeTail();
+		list.list();
+				
+		System.out.println("Remove element ");
+		list.remove(14);
+		list.list();
+		
+		System.out.println("Remove after element ");
+		list.remove(16);
 		list.list();
 
 	}
